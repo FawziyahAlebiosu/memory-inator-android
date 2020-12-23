fc5b617cff0b650dfe8613622bf24f502a493899
fawziyah <fawziyah.alebiosu@gmail.com>
2020-12-23 16:31:57 -0500

    added references to views in activity

--- app/src/main/java/com/androidexperiments/memory_inator/MainActivity.java
+++ app/src/main/java/com/androidexperiments/memory_inator/MainActivity.java
@@ -1,16 +1,25 @@
 package com.androidexperiments.memory_inator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvDisplayPairs;
+    private TextView tvDisplayMoves;
+    private RecyclerView rvImageSpace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDisplayMoves = findViewById(R.id.tvShowMoves);
+        tvDisplayPairs = findViewById(R.id.tvShowPairs);
+        rvImageSpace = findViewById(R.id.rvImagesSpace);
+
     }
 }
