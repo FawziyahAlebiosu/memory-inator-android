ff40b8bd913068fd77af6b1dc13f070434fc6cb5
fawziyah <fawziyah.alebiosu@gmail.com>
2020-12-23 22:18:45 -0500

    recycler view works for dummy data!!

--- app/src/main/java/com/androidexperiments/memory_inator/MainActivity.java
+++ app/src/main/java/com/androidexperiments/memory_inator/MainActivity.java
@@ -1,17 +1,24 @@
 package com.androidexperiments.memory_inator;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView tvDisplayPairs;
     private TextView tvDisplayMoves;
     private RecyclerView rvImageSpace;
+    private List<memory> mIcons;
+    private Integer[] test_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,18 @@ public class MainActivity extends AppCompatActivity {
         tvDisplayMoves = findViewById(R.id.tvShowMoves);
         tvDisplayPairs = findViewById(R.id.tvShowPairs);
         rvImageSpace = findViewById(R.id.rvImagesSpace);
+        test_list= new Integer[] { R.drawable.default_image};
+        //create adapter for rv
+        gridIconAdapter imageAdapter = new gridIconAdapter(this, 8, test_list);
 
 
+
+
+
+        rvImageSpace.setAdapter(imageAdapter);
+        //simply for optimization
+        rvImageSpace.setHasFixedSize(true);
+
+        rvImageSpace.setLayoutManager(new GridLayoutManager(this, 3));
     }
 }
--- app/src/main/java/com/androidexperiments/memory_inator/gridIconAdapter.java
+++ app/src/main/java/com/androidexperiments/memory_inator/gridIconAdapter.java
@@ -1,6 +1,7 @@
 package com.androidexperiments.memory_inator;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +11,28 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+//get list of items you want to display in rv
+//populate it into the rv using adapters
 
-public class gridIconAdapter extends RecyclerView.Adapter<gridIconAdapter.ViewHolder>{
-    private  List<memory> mIcons;
+public class gridIconAdapter  extends RecyclerView.Adapter<gridIconAdapter.ViewHolder>{
+    public  List<memory> mIcons;
+    public Integer[] test_list;
+    public Integer num_items;
+    //get reference for all the views it will hold in here
 
-    public gridIconAdapter(List<memory> mIcons){
-        this.mIcons = mIcons;
+    //mIcons.add(R.drawable.default_image);
+
+
+    public gridIconAdapter(Context context,Integer num_items, Integer[] test_list){
+        this.test_list = test_list;
+        this.num_items = num_items;
     }
     public class ViewHolder extends RecyclerView.ViewHolder{
-        //get reference for all the views it will hold in here
         public ImageView ivIcons;
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivIcons = itemView.findViewById(R.id.ivGridIcons);
+
 
 
         }
@@ -44,12 +53,16 @@ public class gridIconAdapter extends RecyclerView.Adapter<gridIconAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull gridIconAdapter.ViewHolder holder, int position) {
+            //memory current_item = mIcons.get(position);
 
+            ImageView current_image = holder.ivIcons;
+            current_image.setImageResource(test_list[0]);
+            Log.i("haha", "worked");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return num_items;
     }
 }
