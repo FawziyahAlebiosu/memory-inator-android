be5bb74100103829a198fbd2104f3556b3435e1c
fawziyah <fawziyah.alebiosu@gmail.com>
2020-12-24 19:10:59 -0500

    fixed rv overscroll and margin display

--- app/src/main/java/com/androidexperiments/memory_inator/gridIconAdapter.java
+++ app/src/main/java/com/androidexperiments/memory_inator/gridIconAdapter.java
@@ -18,6 +18,8 @@ public class gridIconAdapter  extends RecyclerView.Adapter<gridIconAdapter.ViewH
     public  List<memory> mIcons;
     public Integer[] test_list;
     public Integer num_items;
+    private static final int margin = 10;
+
     //get reference for all the views it will hold in here
 
     //mIcons.add(R.drawable.default_image);
@@ -41,11 +43,22 @@ public class gridIconAdapter  extends RecyclerView.Adapter<gridIconAdapter.ViewH
     @NonNull
     @Override
     public gridIconAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int widthCard = parent.getWidth()/2 - (2* margin);
+        int heightCard = parent.getHeight()/4 -(2* margin);
+        int cardLength = Math.min(widthCard, heightCard);
+
+
+
         Context context = parent.getContext();
         LayoutInflater layoutInflater = LayoutInflater.from(context);
 
         //inflate it
         View iconView = layoutInflater.inflate(R.layout.grid_layout_for_icons, parent, false);
+        ViewGroup.MarginLayoutParams paramsObject = (ViewGroup.MarginLayoutParams) iconView.findViewById(R.id.ivGridIcons).getLayoutParams();
+        paramsObject.width = cardLength;
+        paramsObject.height = cardLength;
+        paramsObject.setMargins(margin, margin, margin, margin);
+
         //return a new object of vewiholder right after
         ViewHolder viewHolder = new ViewHolder(iconView);
         return viewHolder;
@@ -58,8 +71,16 @@ public class gridIconAdapter  extends RecyclerView.Adapter<gridIconAdapter.ViewH
             ImageView current_image = holder.ivIcons;
             current_image.setImageResource(test_list[0]);
             Log.i("haha", "worked");
+            current_image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("tag", "worked");
+                }
+            });
 
-    }
+        }
+
+
 
     @Override
     public int getItemCount() {
